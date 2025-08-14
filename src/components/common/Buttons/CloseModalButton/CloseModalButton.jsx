3c46cd1baa40811542de2232c697a09d2bbd74cf
Luchi <luli79302@gmail.com>
2025-08-14 09:50:00 +0200

    Refactorización del CloseModalButton con los cambios de firebase

--- src/components/common/Buttons/CloseModalButton/CloseModalButton.jsx
+++ src/components/common/Buttons/CloseModalButton/CloseModalButton.jsx
@@ -3,34 +3,11 @@ import PropTypes from "prop-types";
 import { StyledCloseButton } from "./StyledCloseModalButton";
 import closeIcon from "../../../../assets/close.png";
 
-function CloseModalButton({ onClose, updateSelectedRoutine, warmUpExercises }) {
-  const handleClose = () => {
-    if (updateSelectedRoutine && warmUpExercises) {
-      updateSelectedRoutine((routine) => {
-        const selectedWarmUp = warmUpExercises.find(
-          (ex) => ex.name === routine.warmUp
-        );
-
-        const cleanedRoutine = { ...routine };
-
-        if (selectedWarmUp?.type === "timed") {
-          delete cleanedRoutine.warmUpSets;
-          delete cleanedRoutine.warmUpReps;
-        } else if (selectedWarmUp?.type === "reps_sets") {
-          delete cleanedRoutine.warmUpTime;
-        }
-
-        return cleanedRoutine;
-      });
-    }
-
-    onClose();
-  };
-
+function CloseModalButton({ onClose }) {
   return (
     <StyledCloseButton
       aria-label="Cerrar modal"
-      onClick={handleClose}
+      onClick={onClose}
       style={{
         background: "none",
         border: "none",
@@ -46,8 +23,6 @@ function CloseModalButton({ onClose, updateSelectedRoutine, warmUpExercises }) {
 
 CloseModalButton.propTypes = {
   onClose: PropTypes.func.isRequired,
-  updateSelectedRoutine: PropTypes.func,
-  warmUpExercises: PropTypes.array,
 };
 
 export default CloseModalButton;
